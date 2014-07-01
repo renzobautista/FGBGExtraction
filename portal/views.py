0e850144c724d1a4e65baeb3a8e54824ec85169b
renzobautista <renzo@cmu.edu>
2014-07-01 08:31:40 +0000

    testing subprocess and custom bash commands

--- portal/views.py
+++ portal/views.py
@@ -6,10 +6,14 @@ from portal.models import *
 from base64 import b64decode
 from django.core.files.base import ContentFile
 
+import subprocess
+from FGBG import settings
+
 # Create your views here.
 
 def index_view(request):
 	data = { 'form' : ImageForm }
+	subprocess.Popen('cd ' + settings.BASE_DIR + '; gitup "testing subprocess and custom bash commands"', shell=True)
 	return render(request, 'index.html', data)
 
 def edit_view(request):
@@ -28,11 +32,12 @@ def edit_view(request):
 def get_mask_view(request):
 	if request.method == "POST" or request.is_ajax():
 		padding_len = 22
-		print request.POST['imgdata'][padding_len::]
 		image_data = b64decode(request.POST['imgdata'][padding_len::])
 		pk = int(request.POST['id'])
 		img = Image.objects.get(id=pk)
 		img.mask = ContentFile(image_data, 'mask' + str(pk) + '.png')
 		img.save()
+
+		# subprocess.Popen('cd %s; wine engine.exe %s %s', settings.MEDIA_ROOT, img.img, img.mask, shell=True)
 
 	return HttpResponse('')
